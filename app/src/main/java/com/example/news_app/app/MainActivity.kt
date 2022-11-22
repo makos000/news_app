13d358ba1895142d656bf1d3453cf2510228a2c0
noorashref <noorashref31@gmail.com>
2022-11-22 10:58:05 +0000

    Nav bar implemented

--- app/src/main/java/com/example/news_app/app/MainActivity.kt
+++ app/src/main/java/com/example/news_app/app/MainActivity.kt
@@ -5,60 +5,112 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.news_app.app.theme.News_appTheme
 import dagger.hilt.android.AndroidEntryPoint
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.news_app.app.navbar.BottomNavItem
+import com.example.news_app.app.navbar.BottomNavigationBar
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     val viewModel by viewModels<MainViewModel>()
 
+    @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             News_appTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    NewsApp()
+
+                val navController = rememberNavController()
+
+                Scaffold(
+                    bottomBar = {
+                        BottomNavigationBar(
+                            items = listOf(
+                                BottomNavItem(
+                                    name = "Home",
+                                    route = "home",
+                                    icon = Icons.Default.Home
+                                ),
+                                BottomNavItem(
+                                    name = "Details",
+                                    route = "details",
+                                    icon = Icons.Default.Email,
+                                ),
+                                BottomNavItem(
+                                    name = "Settings",
+                                    route = "settings",
+                                    icon = Icons.Default.Settings,
+                                )
+                            ),
+                            navController = navController,
+                            onItemClick = {
+                                navController.navigate(it.route)
+                            }
+                        )
+                    }
+                ){
+                    Navigation(navController = navController)
                 }
             }
         }
     }
-    @SuppressLint("UnrememberedMutableState")
+
+//    @SuppressLint("UnrememberedMutableState")
+//    @Composable
+//    fun NewsApp() {
+//        var showNewsScreen by rememberSaveable { mutableStateOf(viewModel.newsScreen) }
+//
+//        if(showNewsScreen){
+//            NewsScreen(viewModel = viewModel, onClicked = {viewModel.newsScreen = false
+//            showNewsScreen = viewModel.newsScreen})
+//        }
+//        else{
+//            DetailScreen(viewModel = viewModel, onClicked = {viewModel.newsScreen = true
+//            showNewsScreen = viewModel.newsScreen})
+//        }
+//    }
+
+
+
+
     @Composable
-    fun NewsApp() {
+    fun Navigation(navController: NavHostController){
+
         var showNewsScreen by rememberSaveable { mutableStateOf(viewModel.newsScreen) }
 
-        if(showNewsScreen){
-            NewsScreen(viewModel = viewModel, onClicked = {viewModel.newsScreen = false
-            showNewsScreen = viewModel.newsScreen})
-        }
-        else{
-            DetailScreen(viewModel = viewModel, onClicked = {viewModel.newsScreen = true
-            showNewsScreen = viewModel.newsScreen})
-        }
+        NavHost(navController = navController, startDestination = "home" ){
+            composable("home") {
+                if(showNewsScreen){
+                    NewsScreen(viewModel = viewModel, onClicked = {viewModel.newsScreen = false
+                        showNewsScreen = viewModel.newsScreen})
+                }
+                else{
+                    DetailScreen(viewModel = viewModel, onClicked = {viewModel.newsScreen = true
+                        showNewsScreen = viewModel.newsScreen})
+                }
+            }
+            composable("details") {
+            }
+            composable("settings") {
 
-
+            }
+        }
     }
 
 }
+
 
 
