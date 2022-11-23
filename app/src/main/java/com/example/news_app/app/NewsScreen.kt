ec5330b24f1fcf1d908c383204952d45e4df9d5a
makos000 <sarka.businessinquiries@gmail.com>
2022-11-23 15:12:37 +0100

    Innitial commit for UI upgrade branch

--- app/src/main/java/com/example/news_app/app/NewsScreen.kt
+++ app/src/main/java/com/example/news_app/app/NewsScreen.kt
@@ -15,6 +15,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+//Upgrade here
+
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun NewsScreen(viewModel: MainViewModel, onClicked: () -> Unit) {
