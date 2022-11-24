99841fa89a0457bb346dd5ba3567fd72e794eebf
tesfahun <y.w.tesfahun@gmail.com>
2022-11-24 17:29:14 +0000

    ui updated

--- app/src/main/java/com/example/news_app/app/authentication/common/composable/CardComposable.kt
+++ app/src/main/java/com/example/news_app/app/authentication/common/composable/CardComposable.kt
@@ -1,18 +1,3 @@
-/*
-Copyright 2022 Google LLC
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
- */
 
 package com.example.news_app.app.authentication.common.composable
 
@@ -27,7 +12,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.example.news_app.app.authentication.common.ext.dropdownSelector
 
 @ExperimentalMaterialApi
 @Composable
--- app/src/main/java/com/example/news_app/app/authentication/common/composable/ToolbarComposable.kt
+++ app/src/main/java/com/example/news_app/app/authentication/common/composable/ToolbarComposable.kt
@@ -1,18 +1,3 @@
-/*
-Copyright 2022 Google LLC
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
- */
 
 package com.example.news_app.app.authentication.common.composable
 
@@ -54,5 +39,5 @@ fun ActionToolbar(
 
 @Composable
 private fun toolbarColor(darkTheme: Boolean = isSystemInDarkTheme()): Color {
-  return if (darkTheme) MaterialTheme.colors.secondary else MaterialTheme.colors.primaryVariant
+  return if (darkTheme) MaterialTheme.colors.secondary else MaterialTheme.colors.secondaryVariant
 }
